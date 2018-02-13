076fcb145f052bb424fcd2e4ed195ababecc142c
Jaewook Woo <jaewookwoo1@gmail.com>
2018-02-13 09:43:38 +0100

    Initial Commit

---
+++ band/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  band
+//
+//  Created by Jaewook Woo on 13/02/2018.
+//  Copyright © 2018 Jaewook Woo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
