470055bc94f8d904550775f6ce7bb08cc92f95d1
Jaewook Woo <jaewookwoo1@gmail.com>
2018-02-18 15:55:01 +0100

    Add ensemble.cpp

---
+++ band/ensemble.hpp
@@ -0,0 +1,14 @@
+//
+//  ensemble.hpp
+//  band
+//
+//  Created by Jaewook Woo on 18/02/2018.
+//  Copyright © 2018 Jaewook Woo. All rights reserved.
+//
+
+#ifndef ensemble_hpp
+#define ensemble_hpp
+
+#include <stdio.h>
+
+#endif /* ensemble_hpp */
